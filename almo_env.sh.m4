7fe1b45e7392566065ce5436db5d372acf02a58e
jeremiejig <me@jeremiejig.fr>
2014-10-05 15:22:56 +0200

    adding installation file.

---
+++ almo_env.sh.m4
@@ -0,0 +1,9 @@
+# bin directory
+export PATH=INSTALL_DIR/bin:$PATH
+
+# giet directory
+export GIET=INSTALL_DIR/sesi-almo/soft/giet
+
+# gmp and mpfr (for cross-gcc)
+export LD_LIBRARY_PATH=INSTALL_DIR/bin/mipsel-unknown-elf/lib:$LD_LIBRARY_PATH
+
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd $(dirname $0)
+WORKDIR=$(pwd)
+
+echo 'Creating almo_env.sh'
+m4 -D INSTALL_DIR=$WORKDIR $WORKDIR/almo_env.sh.m4 > $WORKDIR/almo_env.sh
+
+echo "Appending 'source $WORKDIR/almo_env.sh in ~/.bashrc'"
+echo "\n#almo env\nsource $WORKDIR/almo_env.sh" >> $HOME/.bashrc
