76f0959a22fc9dd575ea5c75782539255c4aa744
jeremiejig <me@jeremiejig.fr>
2014-10-08 13:51:49 +0200

    new install script, will update bashrc if it detects an old installation

--- install.sh
+++ install.sh
@@ -6,5 +6,10 @@ WORKDIR=$(pwd)
 echo 'Creating almo_env.sh'
 m4 -D INSTALL_DIR=$WORKDIR $WORKDIR/almo_env.sh.m4 > $WORKDIR/almo_env.sh
 
-echo "Appending 'source $WORKDIR/almo_env.sh in ~/.bashrc'"
-echo "\n##almo env\nsource $WORKDIR/almo_env.sh\n\n##fin almo env" >> $HOME/.bashrc
+if grep -q li312_simul_path ~/.bashrc; then
+  echo "old installation detected, updating to source $WORKDIR/almo_env.sh"
+  sed -e "s%^\(li312_simul_path=\).*$%\1'$WORKDIR'%" -i ~/.bashrc
+else
+  echo "Appending 'source $WORKDIR/almo_env.sh in ~/.bashrc'"
+  echo "\n# almo env\nli312_simul_path='$WORKDIR'\nsource \$li312_simul_path/almo_env.sh\n" >> $HOME/.bashrc
+fi;
