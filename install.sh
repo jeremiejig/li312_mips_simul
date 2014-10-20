e9fdea5aa1d1ffe6b39be2cedf00607481859f4b
jeremiejig <me@jeremiejig.fr>
2014-10-20 16:43:41 +0200

    changing install.sh to bash
    using bash script. echo to echo -e

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 cd $(dirname $0)
 WORKDIR=$(pwd)
@@ -7,9 +7,9 @@ echo 'Creating almo_env.sh'
 sed -e "s%INSTALL_DIR%$WORKDIR%" $WORKDIR/almo_env.sh.m4 > $WORKDIR/almo_env.sh
 
 if grep -q li312_simul_path ~/.bashrc; then
-  echo "old installation detected, updating to source $WORKDIR/almo_env.sh"
+  echo -e "old installation detected, updating to source $WORKDIR/almo_env.sh"
   sed -e "s%^\(li312_simul_path=\).*$%\1'$WORKDIR'%" -i ~/.bashrc
 else
-  echo "Appending 'source $WORKDIR/almo_env.sh in ~/.bashrc'"
+  echo -e "Appending 'source $WORKDIR/almo_env.sh in ~/.bashrc'"
   echo "\n# almo env\nli312_simul_path='$WORKDIR'\nsource \$li312_simul_path/almo_env.sh\n" >> $HOME/.bashrc
 fi;
